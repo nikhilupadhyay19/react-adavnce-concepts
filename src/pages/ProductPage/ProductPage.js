fc6baf10ede44697883449ad2eabae206dd05410
nikhilupadhyay19 <nikhilupadhyay19@gmail.com>
2022-07-18 16:36:00 +0530

    1 file modified

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -5,11 +5,12 @@ const TIMEOUT = 10;
 
 const ProductPage = (props) => {
   const [products, setProducts] = useState([]);
+  const [cProducts, setCProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const { name, id } = props;
 
-  console.log('Contructor ..... Rum only once at mounting pahse...');
+  // console.log('Contructor ..... Rum only once at mounting pahse...');
 
   const timeout = (s) => {
     return new Promise((_, reject) => {
@@ -32,47 +33,55 @@ const ProductPage = (props) => {
     }
   };
 
-  // useEffect(() => {
-  //   console.log(
-  //     'Component Did Mount.... Contain blank array array as dependency... Run only once...'
-  //   );
-  //   const getProducts = async () => {
-  //     try {
-  //       const products = await getJSON('https://restcountries.com/v3.1/all');
-  //       setProducts((prevState) => {
-  //         return (prevState = products);
-  //       });
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  //   getProducts();
-  // }, []);
+  useEffect(() => {
+    // console.log(
+    //   'Component Did Mount.... Contain blank array array as dependency... Run only once...'
+    // );
+    console.log(products);
+    const getProducts = async () => {
+      try {
+        const products = await getJSON('https://restcountries.com/v3.1/all');
+        setProducts((prevState) => {
+          return (prevState = products);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, []);
 
   // useEffect(() => {
-  //   console.log(
-  //     'Component Did update... Contain array with dependency... run only when there is a change in the dependency..'
-  //   );
+  //   // console.log(
+  //   //   'Component Did update... Contain array with dependency... run only when there is a change in the dependency..'
+  //   // );
   //   setIsLoading(!isLoading);
   // }, [products]);
 
-  const showdetailsHandler = (id) => {
-    const cProducts = [...products];
-    const index = cProducts.findIndex((el) => el.cca3 === id);
-    cProducts.splice(index, 1);
-    setProducts(cProducts);
-  };
+  // const showdetailsHandler = (id) => {
+  //   const cProducts = [...products];
+  //   const index = cProducts.findIndex((el) => el.cca3 === id);
+  //   cProducts.splice(index, 1);
+  //   setUProducts(cProducts);
+  // };
 
-  console.log('Render...');
+  // useEffect(() => {
+  //   return setProducts((prevState) => {
+  //     return (prevState = cProducts);
+  //   });
+  // }, [cProducts]);
+
+  // console.log('Render...');
 
   return (
     <div className="product-page" id={id}>
       Welcome to the {name}...
-      {isLoading ? (
+      {/* {isLoading ? (
         <p>Please wait while the products has beem loaded....</p>
       ) : (
         <Product data={products} showdetailsHandler={showdetailsHandler} />
-      )}
+      )} */}
+      <Product data={products} />
     </div>
   );
 };

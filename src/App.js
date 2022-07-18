a83e5ffbaf46165790bd24957d2ca2f847960ef1
nikhilupadhyay19 <nikhilupadhyay19@gmail.com>
2022-07-18 11:42:42 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import ProductPage from './pages/ProductPage/ProductPage';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ProductPage />
     </div>
   );
 }
---
+++ src/pages/ProductPage/ProductPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductPage = () => {
+  return <div className="product-page">Welcome to the Product Page...</div>;
+};
+
+export default ProductPage;
